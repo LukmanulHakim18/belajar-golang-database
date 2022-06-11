ef6c2c300f284205c128dd2c99648efc4de0802e
LukmanulHakim18 <lukman.virg@gmail.com>
2022-06-12 01:41:10 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Description
+
+repo ini saya buat untuk belajar database pada golang, saya menggunakan `docker-compose.yaml` untuk mempermudah setup databasenya, sedangkan database nya menggunakan `postgresql`
+adapun yang telah saya pelajari sebagai berikut:
+
+- Membuat connection database
---
+++ database_test.go
@@ -0,0 +1,26 @@
+package golang_database
+
+import (
+	"database/sql"
+	"fmt"
+	"testing"
+
+	_ "github.com/lib/pq"
+)
+
+var (
+	host     = "localhost"
+	port     = 5432
+	user     = "postgres"
+	password = "postgres"
+	dbname   = "belajar_golang_database"
+)
+
+func TestEmpty(t *testing.T) {
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
+	db, err := sql.Open("postgres", psqlInfo)
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+}
